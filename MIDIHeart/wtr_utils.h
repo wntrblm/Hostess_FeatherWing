5eff59a8829a30a15660d770c7d1ac91a6e9246f
Thea Flowers <thea@thea.codes>
2020-06-04 18:48:24 -0700

    s/_print_bytes/print_bytes

--- MIDIHeart/wtr_utils.h
+++ MIDIHeart/wtr_utils.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 #include <stdio.h>
 
-inline void _print_bytes(uint8_t *bytes, size_t len) {
+inline void print_bytes(uint8_t *bytes, size_t len) {
     for (size_t i = 0; i < len; i++) {
         printf("%2x ", bytes[i]);
     }
